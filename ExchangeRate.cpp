517c973ccd953df15cf3326be7cf3b3d49f3fb9e
Puchcharapoom <52170197+Puchcharapoom@users.noreply.github.com>
2019-07-01 19:58:55 +0700

    Add files via upload

---
+++ ExchangeRate.cpp
@@ -0,0 +1,19 @@
+/*Puchcharapoom Mangmool
+ 6206021610111 section : A */
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	
+   unsigned short B ;
+   float $ = 30.71  ;
+   float eu = 35 ;
+
+   cout <<"Enter your monney :";
+   cin >> B;
+   cout << "Your money is" << B << " Bath. " << endl;
+   cout << B <<" Bath is " << B/$ << " Dollars. " << endl;
+   cout << B <<" Bath is " << B/eu << " Euro. " << endl;
+   return 0;
+}
